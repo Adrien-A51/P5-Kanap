04112e6595d14900cefc0ecca3926e441947f555
AdrienA <aabdalalim5@gmail.com>
2022-11-29 17:53:19 +0100

    modifs

--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,19 @@
-//Sélection de la class items
+// Sélectionner la class items
 let items_container = document.querySelector(".items");
 
-// Récupérer les données de l'api avec fetch
-fetch(`http://localhost:3000/api/products/`)
+// Récupérer les données de l'api
+fetch(`http://localhost:3000/api/products`)
   .then((products) => products.json())
   .then((products) => {
     displayProducts(products);
   });
 
-//Création des élèments + rajout des données dans ces balises a article img h3 et p
+//Création des élèments + rajout des données dans ces balises
 function displayProducts(products) {
   items_container.innerHTML = "";
   for (let i = 0; i < products.length; i++) {
     items_container.innerHTML += `
-    <a href="./product.html?id=${products[i]._id}"> 
+    <a href="./product.html?_id=${products[i]._id}"> 
       <article>
         <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
         <h3 class="productName">${products[i].name}</h3> 
