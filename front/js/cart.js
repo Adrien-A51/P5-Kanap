7adc7b34afdeeb10c267da4e4257fcd91fb9eb59
AdrienA <aabdalalim5@gmail.com>
2022-12-09 14:27:09 +0100

    modifs url

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,12 +1,10 @@
 // Déclaration de la variable basket dans laquelle on met les keys et les valeurs qui sont dans le LocalStorage 
 let basket = JSON.parse(localStorage.getItem("produits")); 
 // JSON.parse c'est pour convertir les données au format JSON qui sont dans le Local Storage en objet js
-console.log(basket);
 
 //----------------------------------- Afficher les produits dans le panier ------------------------------------------------
 // Sélection de la classe où Mettre le code html
 const basket_container = document.querySelector("#cart__items"); 
-console.log(basket_container);
 
 // si le panier est vide -> affiche le panier est vide
 // basket === null - basket est strictement égal à null
